3c1542dd1ca155e6533e1742021f41ff2967d256
Jarek Šedý <jareksedy@icloud.com>
2022-02-19 14:17:28 +0600

    Initial Commit

---
+++ SwiftUICombineSlotMachineYS/SwiftUICombineSlotMachineYSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineSlotMachineYSApp.swift
+//  SwiftUICombineSlotMachineYS
+//
+//  Created by Ярослав on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineSlotMachineYSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
