cb36a31177be3fb18799387032924e27a5fc19bd
Jarek Šedý <jareksedy@icloud.com>
2022-02-20 13:40:59 +0600

    Added button text to VM.

--- SwiftUICombineSlotMachineYS/ContentView.swift
+++ SwiftUICombineSlotMachineYS/ContentView.swift
@@ -6,8 +6,43 @@
 //
 
 import SwiftUI
+import Combine
+
+class SlotViewModel: ObservableObject {
+    private var cancellables = Set<AnyCancellable>()
+    private let emojiSourceArray = ["🍋", "🍒", "🦠"]
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    init() {
+        randomize()
+        
+        $running
+            .receive(on: RunLoop.main)
+            .map { $0 == true ? "Стоп!" : "Крутить!" }
+            .assign(to: \.buttonText, on: self)
+            .store(in: &cancellables)
+    }
+    
+    func randomize() {
+        slot1Emoji = emojiSourceArray[Int.random(in: 0...emojiSourceArray.count - 1)]
+        slot2Emoji = emojiSourceArray[Int.random(in: 0...emojiSourceArray.count - 1)]
+        slot3Emoji = emojiSourceArray[Int.random(in: 0...emojiSourceArray.count - 1)]
+    }
+    
+    // input
+    @Published var running = false
+    
+    // output
+    @Published var slot1Emoji = ""
+    @Published var slot2Emoji = ""
+    @Published var slot3Emoji = ""
+    
+    @Published var buttonText = ""
+}
 
 struct ContentView: View {
+    @ObservedObject private var slotViewModel = SlotViewModel()
+    
     var body: some View {
 
         VStack {
@@ -18,24 +53,18 @@ struct ContentView: View {
             Spacer()
             
             HStack {
-                Text("🍋").font(.largeTitle)
+                Text(slotViewModel.slot1Emoji).font(.largeTitle)
                 Spacer().frame(width: 25)
-                Text("🍒").font(.largeTitle)
+                Text(slotViewModel.slot2Emoji).font(.largeTitle)
                 Spacer().frame(width: 25)
-                Text("🌞").font(.largeTitle)
+                Text(slotViewModel.slot3Emoji).font(.largeTitle)
             }
 
             Spacer()
             
-            Button(action: {}, label: { Text("Крутить") })
+            Button(action: { slotViewModel.running.toggle() }, label: { Text(slotViewModel.buttonText) })
             
             Spacer()
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()//.preferredColorScheme(.dark)
     }
 }
